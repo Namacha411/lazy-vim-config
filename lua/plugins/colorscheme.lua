96082f5dc80ffee7adeb996decde2241bb1429e0
Tanaka Hideyuki <thdyk.4.11@gmail.com>
2023-08-21 03:00:11 +0900

    Add files via upload

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "iceberg",
+    },
+  },
+  { "rmehri01/onenord.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  { "bluz71/vim-nightfly-colors", name = "nightfly" },
+  { "rebelot/kanagawa.nvim" },
+  { "olivercederborg/poimandres.nvim" },
+  { "cocopon/iceberg.vim" },
+  { "folke/tokyonight.nvim" },
+}
---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,66 @@
+return {
+  -- Coding
+  -- { "L3MON4D3/LuaSnip", enabled = false},
+  -- { "rafamadriz/friendly-snippets", enabled = false },
+  -- { "hrsh7th/nvim-cmp", enabled = false },
+  -- { "hrsh7th/cmp-nvim-lsp", enabled = false },
+  -- { "hrsh7th/cmp-buffer", enabled = false },
+  -- { "hrsh7th/cmp-path", enabled = false },
+  -- { "saadparwaiz1/cmp_luasnip", enabled = false },
+  -- { "echasnovski/mini.pairs", enabled = false },
+  { "echasnovski/mini.surround", enabled = false },
+  -- { "JoosepAlviste/nvim-ts-context-commentstring", enabled = false },
+  -- { "echasnovski/mini.comment", enabled = false },
+  -- { "echasnovski/mini.ai", enabled = false },
+
+  -- Colorscheme
+  -- { "folke/tokyonight.nvim", enabled = false },
+  -- { "catppuccin/nvim", enabled = false },
+
+  -- Editor
+  { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "nvim-pack/nvim-spectre", enabled = false },
+  -- { "nvim-telescope/telescope.nvim", enabled = false },
+  { "folke/flash.nvim", enabled = false },
+  -- { "folke/which-key.nvim", enabled = false },
+  -- { "lewis6691/gitsigns.nvim", enabled = false },
+  -- { "RRethy/vim-illuminate", enabled = false },
+  -- { "echasnovski/mini.bufremove", enabled = false },
+  { "folke/trouble.nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+
+  -- LSP
+  -- { "neovim/nvim-lspconfig", enabled = false },
+  -- { "folke/neoconf.nvim", enabled = false },
+  -- { "folke/neodev.nvim", enabled = false },
+  -- { "williamboman/mason-lspconfig.nvim", enabled = false },
+  -- { "hr7th/cmp-nvim-lsp", enabled = false },
+  -- { "jose-elias-alvarez/null-ls.nvim", enabled = false },
+  -- { "williamboman/mason.nvim", enabled = false },
+
+  -- TreeSitter
+  -- { "nvim-treesitter/nvim-treesitter", enabled = false },
+  -- { "nvim-treesitter/nvim-treesitter-textobjects", enabled = false },
+
+  -- UI
+  { "rcarriga/nvim-notify", enabled = false },
+  { "stevearc/dressing.nvim", enabled = false },
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- { "nvim-lualine/lualine.nvim", enabled = false },
+  -- { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  -- { "echasnovski/mini.indentscope", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "SmiteshP/nvim-navic", enabled = false },
+  -- { "nvim-tree/nvim-web-devicons", enabled = false },
+  -- { "MunifTanjim/nui.nvim", enabled = false },
+
+  -- Util
+  -- { "dstein64/vim-startuptime", enabled = false },
+  { "folke/persistence.nvim", enabled = false },
+  -- { "nvim-lua/plenary.nvim", enabled = false },
+
+  -- Copilot
+  -- { "zbirenbaum/copilot-cmp", enabled = false },
+  -- { "zbirenbaum/copilot.lua", enabled = false },
+}
